147b5b2968e3166e8c580adc3a9b8e70284b8cf4
a-saveleva <ucfnsav@ucl.ac.uk>
2025-05-23 08:25:26 +0100

    updated config

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,8 @@
-import { defineConfig } from 'vite'
-import ghPages from 'vite-plugin-gh-pages'
+import { defineConfig } from 'vite';
 
 export default defineConfig({
   base: '/casa_0003_group_project/',
-  plugins: [ghPages()]
+  build: {
+    target: 'es2022'
+  }
 });
